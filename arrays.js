a220ba31bdfff70975fb22732614d3af9668cd46
Justin Davis <justin_w_davis@yahoo.com>
2018-10-27 13:41:31 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,10 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray( a, n ) { return [n, ...a] }
+
+//function destructivelyAddElementToBeginningOfArray( a, n ) { return a.unshift( n ) }
+function destructivelyAddElementToBeginningOfArray( a, n ) { return a + n }
+
+function addElementToEndOfArray( a, n ) { return [...a, n] }
+
+function destructivelyAddElementToEndOfArray( a, n ) { return a.push( n ) }
